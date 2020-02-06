a7b434d51c3233301613206106810b76bec93258
Renan Santana <renan_ota@hotmail.com>
2020-02-06 15:53:57 -0300

    Colisões implementadas; Pontuação, vidas e level também.

--- src/js/game.js
+++ src/js/game.js
@@ -19,7 +19,7 @@ boleto.src      = "sprites/boleto.png";
 dedo.src        = "sprites/dedo.png";
 
 // Game variables
-const money_speed   = 5;
+const money_speed   = 3;
 const left_limit    = 20;
 const right_limit   = 340;
 const fire_cooldown = 300;
@@ -27,6 +27,8 @@ const enemy_spawn_x = [20, 84, 148, 212, 276, 340];
 const enemy_spawn_y = -64;
 
 let score           = 0;
+let level           = 0;
+let lives           = 3;
 let is_game_over    = false;
 let player_x        = 20;
 let player_y        = canvas.height - 90;
@@ -91,6 +93,21 @@ function game_over() {
     document.getElementById("game-over").style = "display : inline";
 }
 
+function hit() {
+    lives -= 1;
+    document.getElementById("vidas").innerText = lives;
+}
+
+function increase_score() {
+    score += 1;
+    document.getElementById("score").innerText = score;
+}
+
+function increase_level() {
+    level += 1;
+    document.getElementById("level").innerText = level;
+}
+
 function spawn_enemies() {
     setInterval(() => {
         create_enemy();
@@ -100,6 +117,10 @@ function spawn_enemies() {
 function create_enemy() {
     const position = enemy_spawn_x[Math.floor(Math.random() * enemy_spawn_x.length)];
     boletos.push({"x": position, "y": enemy_spawn_y});
+}
+
+function play_again() {
+
 }
 
 function draw() {
@@ -119,7 +140,22 @@ function draw() {
     for (let i = 0; i < moneys.length; i++) {
         const t_money = moneys[i];
         ctx.drawImage(money, t_money.x, t_money.y);
-        t_money.y -= money_speed
+        t_money.y -= money_speed;
+
+        // Verificar colisões
+        for (let j = 0; j < boletos.length; j++) {
+            const enemy = boletos[j];
+            if (t_money.x - 20 == enemy.x && enemy.y > t_money.y - 40) {
+                boletos.splice(j, 1);
+                moneys.splice(i, 1);
+                increase_score();
+            }
+        }
+
+        // Remover o dinheiro do array ao sair do canvas
+        if (t_money.y < -64) {
+            moneys.splice(i, 1);
+        }
     }
 
     // Desenhar o dedo
@@ -132,6 +168,11 @@ function draw() {
         const t_boleto = boletos[i];
         ctx.drawImage(boleto, t_boleto.x, t_boleto.y);
         t_boleto.y += enemy_speed;
+
+        if (t_boleto.y > canvas.height) {
+            hit();
+            boletos.shift();
+        }
     }
 
     requestAnimationFrame(draw);
